99026ac726cabcd59223cf01cf03b8b2be8ae443
Manisha Jadhav <jadhavmanisha77@gmail.com>
2025-04-23 23:31:34 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,13 +1,16 @@
 import os
 import sys
-# Add the root directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 
 @dataclass
@@ -50,4 +53,9 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
+
 
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,4 +1,6 @@
 import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from dataclasses import dataclass
 
 import numpy as np 
@@ -9,8 +11,9 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
+
 from src.logger import logging
-import os
+
 
 from src.utils import save_object
 
@@ -24,22 +27,23 @@ class DataTransformation:
 
     def get_data_transformer_object(self):
         '''
-        This function si responsible for data trnasformation
+        This function is responsible for data trnasformation
         
         '''
         try:
-            numerical_columns = ["writing_score", "reading_score"]
-            categorical_columns = [
-                "gender",
-                "race_ethnicity",
-                "parental_level_of_education",
-                "lunch",
-                "test_preparation_course",
-            ]
+            numerical_columns = ["network_packet_size",
+                                    "login_attempts",
+                                    "session_duration",
+                                    "ip_reputation_score",
+                                    "failed_logins",
+                                    "unusual_time_access",
+                                    
+                                ]
+            categorical_columns = ["protocol_type", "encryption_used","browser_type"]
 
             num_pipeline= Pipeline(
                 steps=[
-                ("imputer",SimpleImputer(strategy="median")),
+                ("imputer",SimpleImputer(strategy="most_frequent")),
                 ("scaler",StandardScaler())
 
                 ]
@@ -85,8 +89,14 @@ class DataTransformation:
 
             preprocessing_obj=self.get_data_transformer_object()
 
-            target_column_name="math_score"
-            numerical_columns = ["writing_score", "reading_score"]
+            target_column_name="attack_detected"
+            numerical_columns = ["network_packet_size",
+                                    "login_attempts",
+                                    "session_duration",
+                                    "ip_reputation_score",
+                                    "failed_logins",
+                                    "unusual_time_access"
+                                    ]
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
@@ -122,3 +132,4 @@ class DataTransformation:
             )
         except Exception as e:
             raise CustomException(e,sys)
+        
